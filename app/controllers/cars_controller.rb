94a4f61749aa5cb0f16d8d8b3afd55032f8d754e
Andrew Eskenazi <Drewesk@gmail.com>
2016-11-08 23:05:14 -0700

    Manually created CRUD for davinci_motors
    * You can create, view, edit, and destroy cars.
    
    * The price is displayed in $currency format.
    
    * All created spec tests pass.
    
    [#133235215]
    
    Added tests to create and edit a second car
    
    * Creating a new Feature in `creating_cars_spec.rb` with a scenario.
    
    * All tests pass!

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,30 +1,53 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: [:show, :edit, :update, :destroy]
   def index
     @car = Car.all
+  end
+
+  def show
   end
 
   def new
     @car = Car.new
   end
 
+  def edit
+  end
+
   def create
-    car = Car.new(car_params)
-    message = "#{car.year} #{car.make} #{car.model} created"
-    redirect_to root_path,
-      notice: message
+    @car = Car.new(car_params)
+
+    if @car.save
+      created_message = "#{@car.year} #{@car.make} #{@car.model} created"
+      redirect_to root_path,
+        notice: created_message
+    else
+      error_message = "#{@car.errors.full_messages}"
+      redirect_to root_path,
+        notice: error_message
+    end
   end
 
   def update
+    updated_message = 'Car was successfully updated'
+    @car.update(car_params)
+    redirect_to root_path,
+      notice: updated_message
   end
 
   def destroy
+    @car.destroy
+    redirect_to cars_url,
+      notice: 'Car was successfully destroyed'
   end
+
+  private
 
   def car_params
     params.require(:car).permit(:make, :model, :year, :price)
   end
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
 end
-
-
-
-
--- spec/features/creating_cars_spec.rb
+++ spec/features/creating_cars_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
-feature 'Creating Cars' do
-  scenario 'can create a car' do
-    visit '/'
-
-    click_link 'New Car'
+feature 'Creating a Car' do
+  scenario 'User creates car' do
+    visit '/cars/new'
 
     fill_in 'Make', with: 'Ford'
     fill_in 'Model', with: 'Mustang'
@@ -14,15 +12,75 @@ feature 'Creating Cars' do
     click_button 'Create Car'
 
     expect(page).to have_content('1967 Ford Mustang created')
+    end
+
+  scenario 'User edits an existing car' do
+    edited_car = FactoryGirl.create(:car)
+
+    visit "/cars/#{edited_car.id}/edit"
+
+    fill_in 'Make', with: 'Ford'
+    fill_in 'Model', with: 'Mustang'
+    fill_in 'Year', with: '1967'
+    fill_in 'Price', with: '2300'
+
+    click_button 'Update'
+
+    expect(page).to have_content('Car was successfully updated')
   end
-  # scenario 'Car has been created' do
-  #   car = Car.FactoryGirl.create(:car)
-  #
-  #   visit '/'
-  #
-  #   expect(page).to have_content(car.make)
-  #   expect(page).to have_content(car.model)
-  #   expect(page).to have_content(car.year)
-  #   expect(page).to have_content(car.price)
-  # end
+
+  scenario 'User views a car' do
+
+    viewed_car = FactoryGirl.create(:car)
+
+    visit "/cars/#{viewed_car.id}"
+
+    expect(page).to have_content(viewed_car.make)
+    expect(page).to have_content(viewed_car.model)
+    expect(page).to have_content(viewed_car.year)
+    expect(page).to have_content(viewed_car.price)
+  end
+
+  scenario 'User deletes a car' do
+
+    deleted_car = FactoryGirl.create(:car)
+
+    visit '/cars'
+
+    click_link "Destroy"
+
+    expect(page).to_not have_content(deleted_car.make)
+    expect(page).to_not have_content(deleted_car.model)
+    expect(page).to_not have_content(deleted_car.year)
+    expect(page).to_not have_content(deleted_car.price)
+  end
 end
+
+feature do
+  'Creating a Second Car'
+  scenario 'User creates and edits a second car' do
+    visit '/cars/new'
+
+    fill_in 'Make', with: 'Bugatti'
+    fill_in 'Model', with: 'Veyron'
+    fill_in 'Year', with: '2016'
+    fill_in 'Price', with: '222300'
+
+    click_button 'Create Car'
+
+    expect(page).to have_content('2016 Bugatti Veyron created')
+
+    click_link 'Edit'
+
+    fill_in 'Make', with: 'Bugatti'
+    fill_in 'Model', with: 'Veyron'
+    fill_in 'Year', with: '2016'
+    fill_in 'Price', with: '222300'
+
+    click_button 'Update'
+
+    expect(page).to have_content('Car was successfully updated')
+  end
+end
+
+
